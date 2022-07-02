1c3c6a00e60610236e88b77ec11232ceaa5af796
Logan <ldavis96@live.com>
2022-07-02 12:13:01 -0400

    initial file setup

---
+++ index.js
@@ -0,0 +1,12 @@
+function add() {
+  // Create element;
+  var div = document.createElement("div");
+
+  // Set some attributes
+  div.style.width = "200px";
+  div.style.height = "200px";
+  div.style.backgroundColor = "red";
+
+  // Append the div to the body
+  document.body.appendChild(div);
+}
