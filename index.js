b8c3c258b3786d5ca9a3858f65a8d72ba6a99a53
Logan <ldavis96@live.com>
2022-07-02 14:23:23 -0400

    hover affect set up on divs, currently working on button prompt to reset grid

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 let counter = 1;
 
+let answer = parseInt(
+  prompt("How many squares you would like to have in your Etch-A-Sketch grid? ")
+);
+
 function add() {
   // Create element;
   let container = document.querySelector("#container");
   let div = document.createElement("div");
   div.classList.add("new");
+  div.id = "grid";
 
   // Set  attributes
-  div.style.width = "60px";
-  div.style.height = "60px";
+  div.style.width = "30px";
+  div.style.height = "30px";
 
-  if (counter < 256) {
+  if (counter < answer) {
     counter++;
-    window.setTimeout(add, 1);
+    window.setTimeout(add, 0);
   }
 
   // Append the div to the body
   container.appendChild(div);
 }
+
+add();
