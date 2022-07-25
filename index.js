ac4cc4c4b78865fcd1ab722c94ffb3a54b3da146
logan <ldavis96@live.com>
2022-07-25 18:19:51 -0400

    hover affect in place, going to refactor to include mouseDown() and mouseUp() functions for easier drawing

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ let row = 1;
 let column = 1;
 for (let i = 1; i <= celltot; i++) {
   let cell = document.createElement('div');
-  cell.classList.add("new");
+  cell.setAttribute("onmouseover", "style.backgroundColor = 'black'")
+  cell.setAttribute("onclick", "style.backgroundColor = 'white'")
   cell.style.border = '1px solid black';
   cell.style.gridRow = row;
   cell.style.gridColumn = column;
-  
   column += 1;
   if (column === rowtot + 1) {
     row += 1;
